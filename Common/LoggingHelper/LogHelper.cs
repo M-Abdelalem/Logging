b8f8da40465aa501dbf7f707ffe5abc94f735d2b
mohamed.abdelaleem@excelsystems-eg.com <mohamed.abdelaleem@excelsystems-eg.com>
2022-09-27 10:41:57 +0200

    Add project files.

---
+++ Common/LoggingHelper/LogHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace LoggingHelper
+{
+    public static class ConfigureLog
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogger =>
+           (hostingContext, loggerConfiguration) =>
+           {
+               var env = hostingContext.HostingEnvironment;
+
+               loggerConfiguration.MinimumLevel.Information()
+                   .Enrich.FromLogContext()
+                   .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+                   .Enrich.WithProperty("EnvironmentName", env.EnvironmentName)
+                   .Enrich.WithExceptionDetails()
+                   .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                   .MinimumLevel.Override("System.Net.Http.HttpClient", LogEventLevel.Warning)
+                   .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information);
+
+               if (hostingContext.HostingEnvironment.IsDevelopment())
+               {
+                   loggerConfiguration.MinimumLevel.Override("GloboTicket", LogEventLevel.Debug);
+               }
+
+               var elasticUrl = hostingContext.Configuration.GetValue<string>("Logging:ElasticUrl");
+
+               if (!string.IsNullOrEmpty(elasticUrl))
+               {
+                   loggerConfiguration.WriteTo.Elasticsearch(
+                       new ElasticsearchSinkOptions(new Uri(elasticUrl))
+                       {
+                           AutoRegisterTemplate = true,
+                           AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                           IndexFormat = "Micro-Services-logs-{0:yyyy.MM.dd}",
+                           MinimumLogEventLevel = LogEventLevel.Debug
+                       });
+               }
+           };
+    }
+
+
+}
---
+++ Microservices/order/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Order> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Order
+            {
+                Date = DateTime.Now.AddDays(index),
+                cost=100,
+                id=1
+
+            })
+            .ToArray();
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public Order GetById(int id)
+        {
+            var order = new Order
+            {
+                Date = DateTime.Now,
+                cost = 100,
+                id = id
+            };
+            _logger.LogInformation("order data {@order}",order);
+            return order;
+        }
+        [HttpPost]
+        public bool Post(Order order)
+        {
+            try
+            {
+                _logger.LogInformation("save order:" + order.id);
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
